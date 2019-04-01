da2b03f13f61980f30f22efc2b1aa4c1d1af0488
Vinicius Donadon <vinicius.donadon@sunsetcom.com.br>
2019-04-01 18:56:49 -0300

    Modal

---
+++ 0302-html-e-css-do-projeto/EfeitosNoDom/modules-intro/js/modules/teste.js
@@ -0,0 +1,9 @@
+export function teste1(){
+    console.log('teste1')
+}
+
+export function teste2(){
+    console.log('teste2')
+}
+
+export const senha = 2171271;
---
+++ 0302-html-e-css-do-projeto/EfeitosNoDom/modules-intro/js/script.js
@@ -0,0 +1,12 @@
+import initScrollSuave from './modules/scroll-suave.js';
+import initAnimacaoScroll from './modules/animacao-scroll.js'
+import initAccordion from './modules/accordion.js'
+import initTabNav from './modules/tab-nav.js'
+import * as teste from './modules/teste.js'
+
+teste.teste1();
+teste.teste2();
+initScrollSuave();
+initAnimacaoScroll();
+initAccordion();
+initTabNav();
